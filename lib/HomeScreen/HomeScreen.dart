550890b06f0f5c14ee7e4f666c8a8bb2575bc1c1
Ferdaus0001 <ferdaushossan7788990@gmail.com>
2024-09-14 16:02:47 +0600

    All About Getx Doiloag Alart

--- lib/HomeScreen/HomeScreen.dart
+++ lib/HomeScreen/HomeScreen.dart
@@ -23,8 +23,43 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: Column(
         children: [
+
+          Card(
+
+            elevation: 11,
+            child: ListTile(
+
+              title: Text("GETx Diloag Alart "),
+              subtitle: Text('All About Getx '),
+              onTap: (){
+                Get.defaultDialog(
+
+                  title: 'Deleat Your App ',
+                  middleText: 'Are You Sure Delaeate this app Sir ',
+                  contentPadding: EdgeInsets.all(11),
+                  titlePadding: EdgeInsets.all(11),
+                  textConfirm: 'YES',
+                  textCancel: 'No',
+                  confirmTextColor: Colors.blue,
+                  cancelTextColor: Colors.red,
+                  backgroundColor: Colors.white,
+                  content: Column(
+                    children: [
+                      Text(' HI My Name is Ferdaus Hossan Sojib \n and What is your Name By Friend \n What are U From ')
+                    ],
+                  )
+
+                );
+              },
+            ),
+          ),
+
+
+
+
+
           SizedBox(height: 5,),
-          Center(child: Text('HomeScreen()'))
+
         ],
       ),
       floatingActionButton: FloatingActionButton(
